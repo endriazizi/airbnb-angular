b9befae9e73da3931cd0354bd486a99cd05cdad6
Endri Azizi <endri.azizi@gmail.com>
2018-07-19 21:33:35 +0200

    rental module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
-
 //FOR NEW COMPONENT
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
+
+
+
+//ROUTING
+const routes: Routes = [
+  {path: '', component: RentalComponent},
+  {path: 'temp', component: TempComponent}
+]
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     //FOR NEW COMPONENT
-    HeaderComponent
+    HeaderComponent,
+    RentalComponent,
+    TempComponent,
+    RentalListComponent,
+    RentalListItemComponent
   ],
   imports: [
+    //NEW MODULE IMPORT
+    RouterModule.forRoot(routes),
+  
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bwm-rental-list',
+  templateUrl: './rental-list.component.html',
+  styleUrls: ['./rental-list.component.scss']
+})
+export class RentalListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
